a27fdc6e66d22fb06be4234f5df379b62b8060a2
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-19 02:02:31 -0700

    Removed unused code

--- modmail.js
+++ modmail.js
@@ -11,28 +11,6 @@ const { MessageEmbed } = require('discord.js')
 //Bootup check
 client.once('ready', () => {
 	console.log('Ready!');
-	
-		const path = './runstate.txt'
-
-		
-		  if (fs.existsSync(path)) {
-			//file exists
-			var today = new Date();
-			var date = today.getMonth()+1+'-'+(today.getDate())+'-'+today.getFullYear();
-			var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-			global.dateTime = date+' '+time;
-			const StartupEmbed = new Discord.MessageEmbed()
-			.setColor('#ffa900')
-			.setTitle('Bot Started - Issue Detected')
-			.setDescription(`The bot loaded successfully, but didn't shutdown or restart correctly. Please make sure you shutdown or restart the bot correctly next time.`)
-			.addFields(
-				{ name: 'Current date/time: ', value: dateTime, inline: true },
-			)
-			.setTimestamp()
-			global.modlog = client.channels.cache.get(`${BotLog}`);
-			modlog.send(StartupEmbed);
-			return
-		  }else{
 		  var today = new Date();
 				var date = today.getMonth()+1+'-'+(today.getDate())+'-'+today.getFullYear();
 				var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
@@ -47,9 +25,8 @@ client.once('ready', () => {
 				.setTimestamp()
 				global.modlog = client.channels.cache.get(`${BotLog}`);
 				modlog.send(StartupEmbed);
-				fs.writeFileSync('./runstate.txt', 'running')
 				return;
-				}
+				
 });
 
 process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
