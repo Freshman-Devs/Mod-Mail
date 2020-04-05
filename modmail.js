6c97840bc5e856639cf870c5921f68a19c53edf4
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-04-05 06:06:40 -0700

    Fix version number

--- modmail.js
+++ modmail.js
@@ -6,7 +6,7 @@ const {BotLog, MessageLog, RequirePermissonsToUseDmCommand, StaffRoleID} = requi
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const { MessageEmbed } = require('discord.js')
-global.version = 'DEBUG 3.0.0'
+global.version = '3.0.0'
 
 //Bootup check
 client.once('ready', () => {
