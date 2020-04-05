211e79513452473a7739128173a9e1ad62e5fa9f
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-04-05 03:43:40 -0700

    Added blacklist, added set status command, and fixed bug that crashes the bot when mod mail is closed and an image is sent to the bot

--- modmail.js
+++ modmail.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const { DMcommand, token, EnableIncomingMailCommand, DisableIncomingMailCommand, BlacklistCommand} = require('./config.json');
+const {prefix, DMcommand, token, EnableIncomingMailCommand, DisableIncomingMailCommand, BlacklistCommand, WhitelistCommand,SetStatusCommand} = require('./config.json');
 const { nopermreply, BootSuccessful, DmRespondMessage} = require('./strings.json');
 const {BotLog, MessageLog, RequirePermissonsToUseDmCommand, StaffRoleID} = require('./info.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const { MessageEmbed } = require('discord.js')
-global.version = '3.0.0'
-
+global.version = 'DEBUG 3.0.0'
 
 //Bootup check
 client.once('ready', () => {
@@ -47,12 +46,21 @@ client.on('error', error => {
 
 //Check for direct messages
 client.on('message', message => {
+	try{
 	if (message.author.bot) return;
 	if (message.channel.type == "dm") {
 	if (message.content.startsWith(DMcommand)) return;
 	const messagecontent = message.content;
 	const channel = client.channels.cache.get(MessageLog);
+	if (message.content == ''){
+		message.channel.send('Error: Message content can\'t be empty.');
+		message.channel.send('<@'+message.author.id+'>, you must include some text in your message to send it.');
+		return
+	}
+	
+	//Checks if on blacklist
 	fs.readFile('./blacklist.txt', function(err, data){
+		const blacklistdata = data
 		if(data.includes (message.author.id)){
 		const MessageIncomingRejectedBlacklisted = new Discord.MessageEmbed()
 			.setColor('ff0000')
@@ -64,14 +72,16 @@ client.on('message', message => {
 				inline: false },
 				)
 				.setTimestamp()
-				.setFooter('Mod Mail')
-			message.channel.send(MessageIncomingRejectedBlacklisted);return;
-	}})
+				.setFooter('Mod Mail | Version '+version)
+			message.channel.send(MessageIncomingRejectedBlacklisted);
+			return;
+		}
+	//Checks if Mod Mail is enabled.
 	fs.readFile('./allow-incoming.config', function(err, data){
-		console.log(err)
+		if(err)console.log(err)
 	
 		if (data == 'reject'){
-			const MessageIncomingRejected = new Discord.MessageEmbed()
+		const MessageIncomingRejected = new Discord.MessageEmbed()
 			.setColor('ff0000')
 			.setTitle('Message Rejected')
 			.setDescription('Sorry, "'+ channel.guild.name+ '" is currently not accepting Mod Mail at the moment. Please try again later.')
@@ -81,8 +91,9 @@ client.on('message', message => {
 				inline: false },
 				)
 				.setTimestamp()
-				.setFooter('Mod Mail')
-			message.channel.send(MessageIncomingRejected);return;}
+				.setFooter('Mod Mail | Version '+version)
+		message.channel.send(MessageIncomingRejected);return;}
+		//Manages requirements
 		var today = new Date();
 		var date = today.getMonth()+1+'-'+(today.getDate())+'-'+today.getFullYear();
 		var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
@@ -97,7 +108,20 @@ client.on('message', message => {
 				message.channel.send('<@'+message.author.id+'>, your message can\'t exceed 1024 characters. Please shorten your message.');
 				return
 			}
-		const MessageSentEmbed = new Discord.MessageEmbed()
+			//Checks before sending
+			message.react('✅').then(() => message.react('❌'));
+			message.channel.send('Are you sure you want to send your message?')
+
+	const filter = (reaction, user) => {
+	return ['✅', '❌'].includes(reaction.emoji.name) && user.id === message.author.id;
+	};
+
+	message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+	.then(collected => {
+		const reaction = collected.first();
+
+		if (reaction.emoji.name === '✅') {
+			const MessageSentEmbed = new Discord.MessageEmbed()
 			.setColor('008000')
 			.setTitle('Message Sent')
 			.setDescription('Your message was sent to the "'+ channel.guild.name+ '" server. '+DmRespondMessage)
@@ -109,7 +133,7 @@ client.on('message', message => {
 				value: messagecontent,inline: false },
 				)
 				.setTimestamp()
-				.setFooter('Mod Mail')
+				.setFooter('Mod Mail | Version '+version)
 			message.channel.send(MessageSentEmbed);
 				 
 			const MessageReceivedEmbed = new Discord.MessageEmbed()
@@ -122,18 +146,30 @@ client.on('message', message => {
 					{ name: 'Message ', value: messagecontent, inline: false },
 				)
 				.setTimestamp()
+				.setFooter('Mod Mail | Version '+version)
 				channel.send(MessageReceivedEmbed);
 
 			if (message.attachments.size != '0'){
 				channel.send("Attachments were found.", { files: [`${message.attachments.first().url}`] });
-			}})
+			}
+			return;
+		} else {
+			message.reply('Message was cancelled.');
+			return;
+		}
+		})
+		.catch(collected => {
+			message.reply('Message was cancelled due to no response.');
+			return;
+		});
+		})})
 	}
-
+	}catch(err){console.log(err);return}
 })
 
 //Reply
 client.on('message', message => {
-	if (message.content.startsWith(`${DMcommand}`)){
+	if (message.content.startsWith(`${prefix}${DMcommand}`)){
 	if (message.author.bot)return;
 	if (message.channel.type == 'dm')return;
 	if(RequirePermissonsToUseDmCommand == true){
@@ -144,7 +180,7 @@ client.on('message', message => {
 	console.log('Not DMs')
 	try{
 	console.log('Command detected')
-	const args = message.content.slice(DMcommand.length).split(/ +/);
+	const args = message.content.slice((prefix+DMcommand).length).split(/ +/);
 	console.log(args)
 	args.join(' ')
 	const mentionedmemnber = message.mentions.members.first()
@@ -159,6 +195,7 @@ client.on('message', message => {
 					inline: false },
 				)
 				.setTimestamp()
+				.setFooter('Mod Mail | Version '+version)
 				try{
 					mentionedmemnber.send(ReplyReceived);
 					console.log('Attempted to send.')}catch(err){console.log(err)}
@@ -168,7 +205,7 @@ client.on('message', message => {
 
 //Disable Incoming
 client.on('message', message => {
-	if (message.content.startsWith(`${DisableIncomingMailCommand}`)){
+	if (message.content.startsWith(`${prefix}${DisableIncomingMailCommand}`)){
 		if (message.author.bot)return;
 		if (message.channel.type == 'dm')return;
 		if(RequirePermissonsToUseDmCommand == true){
@@ -184,7 +221,7 @@ client.on('message', message => {
 
 //Allow Incoming
 	client.on('message', message => {
-	if (message.content.startsWith(`${EnableIncomingMailCommand}`)){
+	if (message.content.startsWith(`${prefix}${EnableIncomingMailCommand}`)){
 		if (message.author.bot)return;
 		if (message.channel.type == 'dm')return;
 		if(RequirePermissonsToUseDmCommand == true){
@@ -200,7 +237,7 @@ client.on('message', message => {
 
 //Add to blacklist
 client.on('message', message => {
-	if (message.content.startsWith(`${BlacklistCommand}`)){
+	if (message.content.startsWith(`${prefix}${BlacklistCommand}`)){
 		if (message.author.bot)return;
 		if (message.channel.type == 'dm')return;
 		if(RequirePermissonsToUseDmCommand == true){
@@ -208,10 +245,48 @@ client.on('message', message => {
 
 		}else{message.reply(nopermreply);return;}
 	}
+	fs.readFile('./blacklist.txt', function(err, data){
+		if(data.includes (message.mentions.members.first().id)){message.channel.send(`This user is already on the blacklist.`);return;}else
+		{
 		fs.appendFile('./blacklist.txt', `${message.mentions.members.first().id}\n`, function(err,){})
 		message.channel.send(`<@${message.mentions.members.first().id}> (${message.mentions.members.first().id}) was blacklisted of using <@${client.user.id}>. `)
-		
+		}
+	})
 	}})
 
+//Remove from blacklist
+client.on('message', message => {
+	if (message.content.startsWith(`${prefix}${WhitelistCommand}`)){
+		if (message.author.bot)return;
+		if (message.channel.type == 'dm')return;
+		if(RequirePermissonsToUseDmCommand == true){
+		if (message.member.roles.cache.some(role => role.id === `${StaffRoleID}`)){
+
+		}else{message.reply(nopermreply);return;}}
+	fs.readFile('./blacklist.txt', function(err, data){
+		var data = fs.readFileSync('./blacklist.txt', 'utf-8');
+		if(!data.includes(message.mentions.members.first().id)){message.channel.send('User not found on the blacklist.');return}else{message.channel.send('User is now whitelisted.');}
+		var valuetoremove = message.mentions.members.first().id;
+		var newValue = data.replace((valuetoremove), '');
+		fs.writeFileSync('./blacklist.txt', newValue, 'utf-8');
+	})}
+	})
+
+//Sets bot status
+client.on('message', message => {
+	if (message.author.bot)return;
+	if (!message.content.startsWith(`${prefix}${SetStatusCommand}`))return;
+	if (message.channel.type == 'dm')return;
+	if(RequirePermissonsToUseDmCommand == true){
+	if (message.member.roles.cache.some(role => role.id === `${StaffRoleID}`)){}else{message.reply(nopermreply);return;}}
+	const args = message.content.slice((prefix+SetStatusCommand).length).split(/ +/);
+	const activity = args.join(' ')
+	client.user.setActivity(activity, { type: 'WATCHING' });
+	fs.writeFileSync('./statusmessage.config', activity, 'utf-8');
+	message.channel.send('Bot activity set to `WATCHING '+activity+'`.')
+})
+
 //Login
-client.login(token);;
+client.login(token);
+
+//const args = message.content.slice((prefix+DMcommand).length).split(/ +/);
